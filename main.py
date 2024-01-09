d949c89a92ed6b2f2282ed9791db146976c04ff9
PaisleyPython <148840962+PaisleyPython@users.noreply.github.com>
2024-01-09 11:45:21 +0000

    Create foundation for app

---
+++ main.py
@@ -0,0 +1,30 @@
+# CALORIE CALCULATOR
+
+class CalorieCalculator:
+
+    def weight(self):
+        pass
+
+    def height(self):
+        pass
+
+    def age(self):
+        pass
+
+    def temperature(self):
+        pass
+
+    def calculate():
+        pass
+
+
+class Temperature:
+
+    def country(self):
+        pass
+
+    def city(self):
+        pass
+
+    def get():
+        pass
